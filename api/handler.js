c541e2c2e0c28fde9f55e16aa33604f6777b0671
Christian Beddows <christian.beddows@york.ac.uk>
2020-09-01 12:02:57 +0100

    CFIN-196 Add lambda to return snapshot course data

--- api/handler.js
+++ api/handler.js
@@ -1,18 +1,13 @@
 'use strict';
+const COURSES = require("./courses.snapshot.js");
 
-module.exports.hello = async event => {
+module.exports.courses = async event => {
   return {
     statusCode: 200,
     body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
+      COURSES,
       null,
       2
     ),
   };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
