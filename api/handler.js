eb97c5de0c503533629cbf604dc72e84d958173f
Christian Beddows <christian.beddows@york.ac.uk>
2020-09-07 14:32:21 +0100

    CFIN-159 Call Funnelback API

--- api/handler.js
+++ api/handler.js
@@ -1,13 +1,24 @@
 'use strict';
-const COURSES = require("./courses.snapshot.js");
+const fetch = require('node-fetch');
+const BASE_URL = 'https://www.york.ac.uk/search/?collection=york-uni-courses&form=course-search&profile=_default&query=';
 
 module.exports.courses = async event => {
+
+  let params = event.queryStringParameters;
+
+  let url = `${BASE_URL}${params.query}`
+
+  let response = await fetch(url, {
+    method: "GET",
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+
+  let json = await response.json();
+
   return {
     statusCode: 200,
-    body: JSON.stringify(
-      COURSES,
-      null,
-      2
-    ),
+    body: JSON.stringify(json),
   };
 };
