9d4d230263d1cad21d99814e82dd618aa8f55bcc
Christian Beddows <christian.beddows@york.ac.uk>
2020-09-11 11:16:13 +0100

    CFIN-215 Refactor URL generation into dedicated function

--- api/handler.js
+++ api/handler.js
@@ -1,27 +1,14 @@
 'use strict';
 const fetch = require('node-fetch');
-const BASE_URL = 'https://www.york.ac.uk/search/?collection=york-uni-courses&form=course-search&profile=_default&query=';
+const {URLSearchParams} = require('url');
+const BASE_URL = 'https://www.york.ac.uk/search/';
 
 module.exports.courses = async event => {
   let result = {statusCode: 200, body: null};
 
-  if (!event.queryStringParameters) {
-    result.body = JSON.stringify({results: []});
-    return result;
-  }
-
-  const {search, max, offset} = event.queryStringParameters;
-
-  let url = `${BASE_URL}${search}`;
-
-  if (max) {
-    url += `&num_ranks=${max}`;
-  }
-  if (offset) {
-    url += `&start_rank=${offset}`;
-  }
-
   try {
+    const url = generateUrl(event.queryStringParameters);
+
     const response = await fetch(url, {
       method: "GET",
       headers: {
@@ -38,14 +25,33 @@ module.exports.courses = async event => {
     result.statusCode = 500;
     result.body = JSON.stringify({
         error: 'An error has occurred.'
-    })
+    });
   }
 
   return result;
 };
 
+const generateUrl = parameters => {
+  if (!parameters || !parameters.search) {
+    throw Error();
+  }
+
+  const queryParams = new URLSearchParams("collection=york-uni-courses&form=course-search&profile=_default");
+
+  queryParams.append("query", parameters.search);
+
+  if (parameters.max) {
+    queryParams.append("num_ranks", parameters.max);
+  }
+  if (parameters.offset) {
+    queryParams.append("start_rank", parameters.offset);
+  }
+
+  return `${BASE_URL}?${queryParams.toString()}`;
+};
+
 const checkResponse = response => {
   if (!response.ok) {
     throw Error(response.statusText);
-  }
+  }S
 };
