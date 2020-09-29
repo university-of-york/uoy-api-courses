2497faa1ee7bbf35f725ea55c48ee7c2fe464ba7
Richard Allum <richard.allum@york.ac.uk>
2020-09-29 08:14:06 +0100

    apply changes from code review

--- src/utils/transformResponse.js
+++ src/utils/transformResponse.js
@@ -1,36 +1,37 @@
 /**
  * Generic function to map the response from our Funnelback API so that it matches
  * our OpenAPI specification.
- * @param response {[Object]}
- * @returns {[Object]}
  */
 module.exports.transformResponse = (response) => {
     return response.map(course => {
-        transformDistanceLearning(course);
-        transformDepartment(course);
-        return course;
+        const transformedCourse = course;
+        transformDistanceLearning(transformedCourse);
+        transformDepartment(transformedCourse);
+        return transformedCourse;
     });
 };
 
 
 const transformDistanceLearning = (course) => {
-    if(course && course.distanceLearning) {
-        const distanceLearning = typeof course.distanceLearning === "string"
-          ? course.distanceLearning.toLowerCase()
-          : null;
+    let distanceLearning;
+    if(course) {
+        if (typeof course.distanceLearning === "string") {
+            distanceLearning = course.distanceLearning.toLowerCase();
+        } else {
+            distanceLearning = "no";
+        }
 
-        course.distanceLearning = distanceLearning === "yes" ? true : distanceLearning === "no" ? false : null;
+        course.distanceLearning = distanceLearning === "yes";
     }
 
     return course;
 };
 
 const transformDepartment = (course) => {
-    if(course && course.department) {
-        const departments = typeof course.department === "string"
-            ? course.department
-            : "";
+    if(course.department && typeof course.department === "string") {
+        const departments = course.department;
 
+        //TODO: Business Analysis work to determine if we can change the delimiter for departments
         if(departments.includes(", ")) {
             course.department = departments.split(", ");
         } else {
