962ea74dd4f40e446fc0cefb050f3e66ead7052a
Nicola Normandale <nicola.normandale@york.ac.uk>
2021-05-20 16:26:20 +0100

    CFIN-252 Add audit info to the log entry * Also, updated serverless to use node 14 * Not managing to extract header data from the request yet

--- src/utils/auditLogEntry.js
+++ src/utils/auditLogEntry.js
@@ -1,22 +1,36 @@
 module.exports.logEntry = (event, statuscode) => {
-    return {
-        body: JSON.stringify({
-            timestamp: new Date().toISOString(),
-            req: {
-                user: null,
-                service: "uoy-api-courses",
-            },
-            correlationId: event.apiId,
-            self: {
-                application: "uoy-api-courses",
-                type: event.httpMethod,
-            },
-            statusCode: statuscode,
-            resourceId: null,
-            version: "v1",
-            sensitive: false,
-            schemaURI: "https://github.com/university-of-york/uoy-api-courses/README.md",
-            type: "app",
-        }),
+    const getHeaderInfo = () => {
+        const source = event.headers["X-Forwarded-For"];
+        const sourcePort = event.headers["X-Forwarded-For"];
+        return {
+            source,
+            sourcePort,
+        };
     };
+
+    const headers = getHeaderInfo;
+
+    return JSON.stringify({
+        timestamp: new Date().toISOString(),
+        ip: {
+            client: event?.requestContext?.identity?.sourceIp ? event?.requestContext?.identity?.sourceIp : null,
+            source: headers.source ? headers.source : null,
+            sourcePort: headers.sourcePort ? headers.sourcePort : null,
+        },
+        req: {
+            user: null,
+            service: "uoy-api-courses",
+        },
+        correlationId: event?.requestContext?.apiId ? event.requestContext.apiId : null,
+        self: {
+            application: "uoy-api-courses",
+            type: event?.httpMethod ? event.httpMethod : null,
+            statusCode: statuscode,
+            version: "v1",
+        },
+        sensitive: false,
+        schemaURI: "https://github.com/university-of-york/uoy-api-courses/README.md",
+        type: "audit",
+        queryStringParameters: event.queryStringParameters,
+    });
 };
