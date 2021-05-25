d4f8228057a4a529a0b7209ca36732043fc85090
Nicola Normandale <nicola.normandale@york.ac.uk>
2021-05-25 14:30:12 +0100

    CFIN-252 Remove use of JSON.parse in auditLogEntry.test.js

--- src/utils/auditLogEntry.test.js
+++ src/utils/auditLogEntry.test.js
@@ -3,48 +3,47 @@ const { auditLogEntry } = require("./auditLogEntry");
 
 test("Required data added to audit log when supplied in event object", () => {
     MockDate.set(new Date());
-    const event = JSON.parse(
-        "{\n" +
-            '        "resource": "/courses",\n' +
-            '        "path": "/courses",\n' +
-            '        "httpMethod": "GET",\n' +
-            '        "headers": {\n' +
-            '            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",\n' +
-            '            "Accept-Encoding": "gzip, deflate, br",\n' +
-            '            "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8",\n' +
-            '            "CloudFront-Forwarded-Proto": "https",\n' +
-            '            "CloudFront-Is-Desktop-Viewer": "true",\n' +
-            '            "CloudFront-Is-Mobile-Viewer": "false",\n' +
-            '            "CloudFront-Is-SmartTV-Viewer": "false",\n' +
-            '            "CloudFront-Is-Tablet-Viewer": "false",\n' +
-            '            "CloudFront-Viewer-Country": "GB",\n' +
-            '            "Host": "k0fqbcuftg.execute-api.eu-west-1.amazonaws.com",\n' +
-            '            "sec-ch-ua-mobile": "?0",\n' +
-            '            "sec-fetch-dest": "document",\n' +
-            '            "sec-fetch-mode": "navigate",\n' +
-            '            "sec-fetch-site": "none",\n' +
-            '            "sec-fetch-user": "?1",\n' +
-            '            "upgrade-insecure-requests": "1",\n' +
-            '            "User-Agent":\n' +
-            '                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36",\n' +
-            '            "Via": "2.0 bf4a364e1dd23fe6634f1bf013457c5c.cloudfront.net (CloudFront)",\n' +
-            '            "X-Amz-Cf-Id": "7Px4kURZfIuh4B09Wv6vM5cSpbVXaOv1pW6h0eBMnHPpsJVQ19QtuA==",\n' +
-            '            "X-Amzn-Trace-Id": "Root=1-60a6734a-06f7e1dd276e8dc64be2d129",\n' +
-            '            "X-Forwarded-For": "144.32.90.155, 130.176.97.157",\n' +
-            '            "X-Forwarded-Port": "443",\n' +
-            '            "X-Forwarded-Proto": "https"\n' +
-            "        },\n" +
-            '        "queryStringParameters": {\n' +
-            '            "search": "biology"\n' +
-            "        },\n" +
-            '        "requestContext": {\n' +
-            '            "identity": {\n' +
-            '                "sourceIp": "144.32.90.155"\n' +
-            "            },\n" +
-            '            "apiId": "theApiId"\n' +
-            "        }\n" +
-            "    }"
-    );
+    const event = {
+        resource: "/courses",
+        path: "/courses",
+        httpMethod: "GET",
+        headers: {
+            Accept:
+                "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8",
+            "CloudFront-Forwarded-Proto": "https",
+            "CloudFront-Is-Desktop-Viewer": "true",
+            "CloudFront-Is-Mobile-Viewer": "false",
+            "CloudFront-Is-SmartTV-Viewer": "false",
+            "CloudFront-Is-Tablet-Viewer": "false",
+            "CloudFront-Viewer-Country": "GB",
+            Host: "k0fqbcuftg.execute-api.eu-west-1.amazonaws.com",
+            "sec-ch-ua-mobile": "?0",
+            "sec-fetch-dest": "document",
+            "sec-fetch-mode": "navigate",
+            "sec-fetch-site": "none",
+            "sec-fetch-user": "?1",
+            "upgrade-insecure-requests": "1",
+            "User-Agent":
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36",
+            Via: "2.0 bf4a364e1dd23fe6634f1bf013457c5c.cloudfront.net (CloudFront)",
+            "X-Amz-Cf-Id": "7Px4kURZfIuh4B09Wv6vM5cSpbVXaOv1pW6h0eBMnHPpsJVQ19QtuA==",
+            "X-Amzn-Trace-Id": "Root=1-60a6734a-06f7e1dd276e8dc64be2d129",
+            "X-Forwarded-For": "144.32.90.155, 130.176.97.157",
+            "X-Forwarded-Port": "443",
+            "X-Forwarded-Proto": "https",
+        },
+        queryStringParameters: {
+            search: "biology",
+        },
+        requestContext: {
+            identity: {
+                sourceIp: "144.32.90.155",
+            },
+            apiId: "theApiId",
+        },
+    };
 
     expect(auditLogEntry(event, 200, 66)).toEqual(
         JSON.stringify({
