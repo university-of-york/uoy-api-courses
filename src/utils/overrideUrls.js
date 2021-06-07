6d374023963e451e30a0e59e9b53ebcf023c48cb
Christian Beddows <christian.beddows@york.ac.uk>
2021-06-07 12:08:28 +0100

    CFIN-517 Update function to only override specified URLs

--- src/utils/overrideUrls.js
+++ src/utils/overrideUrls.js
@@ -1,5 +1,8 @@
 const overrideUrl = (url) => {
-    return "https://www.york.ac.uk/study/undergraduate/subjects/nursing/";
+    if (url === "https://www.york.ac.uk/study/undergraduate/courses/bsc-nursing-adult/") {
+        return "https://www.york.ac.uk/study/undergraduate/subjects/nursing/";
+    }
+    return url;
 };
 
 module.exports = { overrideUrl };
--- src/utils/overrideUrls.test.js
+++ src/utils/overrideUrls.test.js
@@ -1,10 +1,16 @@
 const { overrideUrl } = require("./overrideUrls");
 
 describe("overrideUrls", () => {
-    it("does an override", () => {
-
+    it("overrides a URL that should be overridden", () => {
         const liveUrl = "https://www.york.ac.uk/study/undergraduate/courses/bsc-nursing-adult/";
         const expectedUrl = "https://www.york.ac.uk/study/undergraduate/subjects/nursing/";
+
+        expect(overrideUrl(liveUrl)).toEqual(expectedUrl);
+    });
+
+    it("doesn't override a URL that shouldn't be overridden", () => {
+        const liveUrl = "https://www.york.ac.uk/study/undergraduate/courses/bsc-physics/";
+        const expectedUrl = "https://www.york.ac.uk/study/undergraduate/courses/bsc-physics/";
 
         expect(overrideUrl(liveUrl)).toEqual(expectedUrl);
     });
