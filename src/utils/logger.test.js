827ddd8af2c7cb4adeded5db254766671806ac43
rmil <rhys.milling@york.ac.uk>
2021-10-22 18:06:05 +0100

    DAF-106 remove superfluous test

--- src/utils/logger.test.js
+++ src/utils/logger.test.js
@@ -1,9 +1,6 @@
 const { logger } = require("./logger");
 
 describe("logger", () => {
-    it("Is a Pino class", () => {
-        expect(logger.constructor.name).toEqual("Pino");
-    });
     it("No exception is thrown when it can be invoked", () => {
         expect(() => logger.info("test")).not.toThrow();
     });
