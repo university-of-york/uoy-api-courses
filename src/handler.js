9934da999f8415c2a6491d03a970384b83a00020
rmil <rhys.milling@york.ac.uk>
2021-11-29 18:21:27 +0000

    CFIN-586: update `handler` to use only `logEntry`

--- src/handler.js
+++ src/handler.js
@@ -1,11 +1,11 @@
 ("use strict");
 const fetch = require("node-fetch");
 const { coursesUrl } = require("./utils/constructFunnelbackUrls");
-const { logEntry, errorEntry } = require("./utils/logEntry");
+const { logEntry } = require("./utils/logEntry");
 const { success, error } = require("./utils/format");
 const { transformResponse } = require("./utils/transformResponse");
 const { overrideUrls } = require("./utils/overrideUrls");
-const { LOG_TYPES, HTTP_CODES } = require("./constants/constants");
+const { HTTP_CODES } = require("./constants/constants");
 const { logger } = require("./utils/logger");
 const { NoQueryGivenError, FunnelbackError } = require("./constants/errors");
 
@@ -14,12 +14,13 @@ module.exports.courses = async (event) => {
         const requestParams = event.queryStringParameters;
 
         if (!requestParams || !requestParams.search) {
-            const errDetails = {
+            const errorDetails = {
+                funnelBackUrl: null,
                 status: HTTP_CODES.BAD_REQUEST,
                 statusText: "Bad Request",
             };
-            const err = new NoQueryGivenError("The search parameter is required.", errDetails);
-            logger.info(errorEntry(event, err, null));
+            const err = new NoQueryGivenError("The search parameter is required.", errorDetails);
+            logger.info(logEntry(event, null, err));
             return error(err.message, err.details.status, err.details.statusText, event.path);
         }
 
@@ -46,20 +47,24 @@ module.exports.courses = async (event) => {
         let results = transformResponse(body.results);
         results = overrideUrls(results);
 
-        const additionalDetails = { numberOfMatches };
-        logger.info(logEntry(event, searchResponse.status, LOG_TYPES.AUDIT, additionalDetails));
+        logger.info(logEntry(event, { numberOfMatches, statusCode: searchResponse.status }));
 
         return success({ numberOfMatches, results });
     } catch (err) {
-        if (!err.details) {
+        // When there is an unknown error it is likely there is no error details passed, so
+        // we are initialising an empty error details
+        if (!err.details)
             err.details = {
                 funnelBackUrl: null,
                 status: HTTP_CODES.INTERNAL_SERVER_ERROR,
                 statusText: "Internal Server Error",
             };
-        }
 
-        logger.error(errorEntry(event, err, null));
+        if (!err.type) err.type = err.name || "UnknownError";
+
+        if (!err.message) err.message = "Internal Server Error";
+
+        logger.error(logEntry(event, null, err), "Internal Server Error");
         return error(err.message, err.details.status, err.details.statusText, event.path);
     }
 };
